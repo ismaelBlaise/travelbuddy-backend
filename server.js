adabaaad76e453e9678772535c0fbfdf9926dd3b
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-08-15 05:35:23 +0300

    valider

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { sequelize } from './config/database.js';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/user.js';
+import badgeRoutes from './routes/badge.js';
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,7 @@ app.get('/', (req, res) => res.send('Hello TravelBuddy with ES Modules!'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);  
+app.use('/api/badges', badgeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
