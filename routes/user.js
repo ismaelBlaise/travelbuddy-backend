433a6ab06ab21e9ec9be2baf525200c9d16fbf28
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-08-15 04:48:39 +0300

    creation des validation de donene

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authenticate } from '../middlewares/auth.js';
 import { updateProfile, updatePassword,updatePhoto } from '../controllers/userController.js';
+import { validatePassword, validateProfile } from '../middlewares/user.js';
 
 const router = express.Router();
 
-router.put('/profile', authenticate, updateProfile);  
-router.put('/password', authenticate, updatePassword);  
+router.put('/profile', authenticate,validateProfile, updateProfile);  
+router.put('/password', authenticate,validatePassword, updatePassword);  
 router.put('/photo', authenticate, updatePhoto);   
 
 export default router;
